73821ac4bbc42a9ba67258fadec01ca15a77d644
Nick Bennett <nick@nickbenn.com>
2022-01-27 13:42:23 -0700

    Include switch expression.

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive;
 
 import java.util.function.Function;
+import java.util.function.IntFunction;
 
 public class FizzBuzz {
 
@@ -12,17 +13,22 @@ public class FizzBuzz {
   public static void main(String[] args) {
     int limit = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_LIMIT;
     FizzBuzz fb = new FizzBuzz();
-    long duration = fb.measure(limit, fb::checkLadder);
-    System.out.printf("%s: %,d iterations in %d milliseconds = %.6f ms/iteration%n",
-        "Ladder", limit, duration, (double) duration / limit);
-    duration = fb.measure(limit, fb::checkNested);
-    System.out.printf("%s: %,d iterations in %d milliseconds = %.6f ms/iteration%n",
-        "Nested", limit, duration, (double) duration / limit);    duration = fb.measure(limit, fb::checkBuilder);
+    long duration;
+    duration = fb.measure(limit, fb::checkBuilder);
     System.out.printf("%s: %,d iterations in %d milliseconds = %.6f ms/iteration%n",
         "Builder", limit, duration, (double) duration / limit);
+    duration = fb.measure(limit, fb::checkSwitch);
+    System.out.printf("%s: %,d iterations in %d milliseconds = %.6f ms/iteration%n",
+        "Switch", limit, duration, (double) duration / limit);
+    duration = fb.measure(limit, fb::checkNested);
+    System.out.printf("%s: %,d iterations in %d milliseconds = %.6f ms/iteration%n",
+        "Nested", limit, duration, (double) duration / limit);
+    duration = fb.measure(limit, fb::checkLadder);
+    System.out.printf("%s: %,d iterations in %d milliseconds = %.6f ms/iteration%n",
+        "Ladder", limit, duration, (double) duration / limit);
   }
 
-  private long measure(int limit, Function<Integer, String> processor) {
+  private long measure(int limit, IntFunction<String> processor) {
     long start = System.currentTimeMillis();
     for (int i = 1; i <= limit; i++) {
       processor.apply(i);
@@ -48,13 +54,15 @@ public class FizzBuzz {
 
   public String checkNested(int value) {
     String result;
-    if (value % 3 == 0) {
-      if (value % 5 == 0) {
+    boolean divisibleBy3 = value % 3 == 0;
+    boolean divisibleBy5 = value % 5 == 0;
+    if (divisibleBy3) {
+      if (divisibleBy5) {
         result = FIZZ_BUZZ;
       } else {
         result = FIZZ;
       }
-    } else if (value % 5 == 0) {
+    } else if (divisibleBy5) {
       result = BUZZ;
     } else {
       result = String.valueOf(value);
@@ -74,6 +82,15 @@ public class FizzBuzz {
       buffer.append(value);
     }
     return buffer.toString();
+  }
+
+  public String checkSwitch(int value) {
+    return switch (value % 15) {
+      case 0 -> FIZZ_BUZZ;
+      case 3, 6, 9, 12 -> FIZZ;
+      case 5, 10 -> BUZZ;
+      default -> String.valueOf(value);
+    };
   }
 
 }
